012313b10d65528480a9f8fdd5010a1316fe9c3b
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-09-25 19:32:33 +0300

    01. All done 100/100 :)

---
+++ Arrays and Nested Arrays - Lab/01. Even Position Elements.js
@@ -0,0 +1,12 @@
+function solve(input) {
+    let result = '';
+    for (let i = 0; i < input.length; i++) {
+        if (i % 2 !== 0)
+            continue;
+        else
+            result += input[i] + ' ';
+    }
+    console.log(result);
+}
+
+solve([10,20,30,40,50,60]);
