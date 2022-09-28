f7af45bb158103c9a33b1ef284986b58c88363c0
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-09-28 21:57:42 +0300

    Demo for Objects and Composition

--- Objects and Composition - Lab/Demo.js
+++ Objects and Composition - Lab/Demo.js
@@ -4,7 +4,7 @@ let cat = {
     sayMyName() {
         console.log(`My name is ${this.name}`);
     }
-};
+};//Object definiton with name age and sayMyName function which is referencing the object from the context of the function.
 
 let func = cat.sayMyName;//Not working properly since there is no context to the "this." keyword.
 
@@ -12,8 +12,26 @@ let dog = {
     name: 'Ares',
     age: 1,
     sayMyName: func
-}
+}//Another Object definition with name, age and the same function which was not working on its own but in this object with property 'name' is working as it should.
+
+let owner = {
+    firstName:'Nikolai',
+    lastName:'Nachev',
+    age:23,
+    address :{
+        country : "Bulgaria",
+        city:"Sofia",
+        district: "Somewhere"
+    },
+    sayMyAddress(){
+        console.log(`My address is :\n${this.address.country}, ${this.address.city}, ${this.address.district}`);
+    }
+};//Composition is Nested objects.
+
+cat.owner = owner;//Adding the owner with the address object to the cat object for even bigger Composition.
 
 cat.sayMyName();
 func();
 dog.sayMyName();
+
+cat.owner.sayMyAddress();
