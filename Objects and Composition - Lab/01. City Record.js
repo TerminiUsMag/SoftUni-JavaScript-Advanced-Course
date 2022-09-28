04c929ce878bb019923be0a904bc58d1e88e5a43
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-09-28 22:05:01 +0300

    01. All done 100/100 :)

---
+++ Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,10 @@
+function solve(name,population,treasury){
+    return {
+        name,
+        population,
+        treasury
+    }
+}
+
+let newObj = solve("Sofia",1000,10000);
+console.log(newObj);
