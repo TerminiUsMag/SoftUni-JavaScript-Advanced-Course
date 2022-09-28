9b409ccb2604b2930f30cf5cbff8ed1b24e13ae7
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-09-28 22:23:13 +0300

    03.All done 100/100 :)

--- Objects and Composition - Lab/02. Town Population.js
+++ Objects and Composition - Lab/02. Town Population.js
@@ -2,9 +2,16 @@ function solve(towns) {
     let registry = {};
     for (town of towns) {
         let info = town.split(' <-> ');
-        registry[info[0]] = info[1];
+        let name = info[0];
+        let population = Number(info[1]);
+        if (registry[name] !== undefined && registry[name] !== null)
+            registry[name] += population;
+        else
+            registry[name] = population;
     }
-    console.log(registry);
+    for (const [key, value] of Object.entries(registry)) {
+        console.log(`${key} : ${value}`);
+    }
 }
 
 solve(['Sofia <-> 1200000',
