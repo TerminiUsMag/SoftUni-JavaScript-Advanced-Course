ce3df016bf7751572f61a331698bb9642556bfe9
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-10-22 00:04:50 +0300

    finish later

---
+++ JS Advanced Exam Preparation I/03. Book-selection/tests.js
@@ -0,0 +1,11 @@
+let {isGenreSuitable,isItAffordable,suitableTitles} = require('./solution');
+let {should} = require('chai');
+
+describe('isGenreSuitable',function(){
+
+    it('Should eat my dick',function(){
+
+    })
+
+
+});
