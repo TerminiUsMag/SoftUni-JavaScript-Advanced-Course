f59a17941c118ca2f144f37ef5d373093f2e6695
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-10-21 20:00:02 +0300

    05. All done 100/100 :)

---
+++ Advanced Functions - Lab/05. Command Processor.js
@@ -0,0 +1,17 @@
+function solve() {
+    let content = '';
+    return {
+        append: (s) => content += s,
+        removeStart: (n) => content = content.substring(n),
+        removeEnd: (n) => content = content.substring(0, content.length - n),
+        print: () => console.log(content)
+    };
+}
+
+let secondZeroTest = solve();
+
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
